458b723bdbe84d6ab0d06edc03f78611576cfafc
america <dreamers.ball66@gmail.com>
2024-12-20 02:08:11 +0900

    Fix Copy Button

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,10 +1,15 @@
-function copyToClipboard(id, button) {
-  const codeBlock = document.getElementById(id);
-  if (!codeBlock) return;
-    const text = codeBlock.textContent.replace(/^\$\s/gm, "");
-     navigator.clipboard.writeText(text).then(() => {
-     button.textContent = "Copied!";
-  }).catch(err => {
-    console.error("Failed to copy: ", err);
-  });
-}
+        function copyToClipboard(id, button) {
+            const codeBlock = document.getElementById(id);
+            if (!codeBlock) return;
+
+            const text = codeBlock.textContent.replace(/^\$\s/gm, "");
+            navigator.clipboard.writeText(text).then(() => {
+                const originalText = button.textContent;
+                button.textContent = "Copied!";
+                setTimeout(() => {
+                    button.textContent = originalText;
+                }, 2000);
+            }).catch(err => {
+                console.error("Failed to copy: ", err);
+            });
+        }
