8c277da47876006a26c2b52df4fd7a6636e5af8b
america <dreamers.ball66@gmail.com>
2024-12-20 00:47:11 +0900

    Fix Copy Button

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,15 +1,19 @@
-function copyToClipboard(id) {
-    const textToCopy = document.getElementById(id).innerText;
+function copyToClipboard(elementId) {
+    const textToCopy = document.getElementById(elementId).innerText;
     navigator.clipboard.writeText(textToCopy).then(() => {
-        // コピー成功時に「Copied!」を表示
-        const button = document.querySelector(`button[onclick="copyToClipboard('${id}')"]`);
+        console.log("Text copied to clipboard");
+
+        // ボタンを取得
+        const button = document.querySelector(`button[onclick="copyToClipboard('${elementId}')"]`);
+
+        // クラスを追加して「Copied!」を表示
         if (button) {
-            button.innerText = "Copied!";
+            button.classList.add('copied');
             setTimeout(() => {
-                button.innerText = "Copy";
-            }, 2000); // 2秒後に元の「Copy」に戻す
+                button.classList.remove('copied'); // 2秒後に元に戻す
+            }, 2000);
         }
     }).catch(err => {
-        console.error('Failed to copy: ', err);
+        console.error("Failed to copy text: ", err);
     });
 }
